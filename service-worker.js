64fab0591b3970c439bc3e8505e323083948bee7
sjc <shijiachen@hztianque.com>
2017-07-24 19:24:41 +0800

    init project

---
+++ service-worker.js
@@ -0,0 +1,72 @@
+importScripts('js/cache-polyfill.js');
+
+var CACHE_VERSION = 'app-v1';
+var CACHE_FILES = [
+    'service-worker.js',
+    '/jacendemo.github.io/',
+    '/jacendemo.github.io/ppt.html',
+    'js/app.js',
+    'js/impress.js',
+    'images/background.jpeg',
+    'css/reset.css',
+    'css/styles.css',
+    'css/Roboto.woff2',
+    'css/ppt.css',
+    // 'https://fonts.googleapis.com/css?family=Roboto:100',
+];
+
+
+self.addEventListener('install', function (event) {
+    console.log('install', event)
+    event.waitUntil(
+        caches.open(CACHE_VERSION)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(CACHE_FILES);
+            })
+    );
+});
+
+self.addEventListener('activate', function (event) {
+    console.log('activate', event)
+    event.waitUntil(
+        caches.keys().then(function (keys) {
+            return Promise.all(keys.map(function (key, i) {
+                if (key !== CACHE_VERSION) {
+                    return caches.delete(keys[i]);
+                }
+            }))
+        })
+    )
+});
+
+self.addEventListener('fetch', function (event) {
+    console.log('fetch', event)
+    event.respondWith(
+        caches.match(event.request).then(function (res) {
+            if (res) {
+                return res;
+            }
+            requestBackend(event);
+        })
+    )
+});
+
+function requestBackend(event) {
+    console.log('requestBackend', event)
+    var url = event.request.clone();
+    return fetch(url).then(function (res) {
+        //if not a valid response send the error
+        if (!res || res.status !== 200 || res.type !== 'basic') {
+            return res;
+        }
+
+        var response = res.clone();
+
+        caches.open(CACHE_VERSION).then(function (cache) {
+            cache.put(event.request, response);
+        });
+
+        return res;
+    })
+}
